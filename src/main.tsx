bb73d4068e42409d54cc7aaa27c9b4f4ea4a22b9
rahul232007 <rahulesec@gmail.com>
2026-07-27 14:22:51 +0530

    feat: add global fetch interceptor to strip extra S3 presigned URL parameters

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,26 @@
+// --- BULLETPROOF AWS PRESIGNED URL CLEANER ---
+const originalFetch = window.fetch;
+window.fetch = async function (input, init) {
+  let url = typeof input === 'string' ? input : input instanceof Request ? input.url : String(input);
+
+  if (url && url.includes('X-Amz-Algorithm')) {
+    url = url
+      .replace(/&x-amz-checksum-mode=[^&]*/g, '')
+      .replace(/&x-id=[^&]*/g, '')
+      .replace(/\?x-amz-checksum-mode=[^&]*&?/g, '?')
+      .replace(/\?x-id=[^&]*&?/g, '?')
+      .replace(/\?$/, ''); // Clean trailing question mark if empty
+
+    if (typeof input === 'string') {
+      input = url;
+    } else if (input instanceof Request) {
+      input = new Request(url, init);
+    }
+  }
+
+  return originalFetch(input, init);
+};
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@app/App';
